e2e057fa8f889690efb54c709eb447e16261d9fa
Tamo Tkhlashidze <tkhlashidzetamo0@gmail.com>
2016-08-30 23:59:57 +0400

    TEST COMMIT

--- KeirseySorter/Startup.cs
+++ KeirseySorter/Startup.cs
@@ -4,7 +4,7 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(KeirseySorter.Startup))]
 namespace KeirseySorter
 {
-    public partial class Startup
+    public partial class Startup //123
     {
         public void Configuration(IAppBuilder app)
         {
